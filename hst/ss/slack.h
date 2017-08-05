67bc64c69224220d7a6cf6d411682a1cf5b450b2
fepz <franpaez@gmail.com>
2017-08-05 18:07:31 -0300

    Add comments into header file.

--- hst/ss/slack.h
+++ hst/ss/slack.h
@@ -18,13 +18,43 @@ struct TaskInfo_Slack {
 
 typedef struct TaskInfo_Slack TaskSs_t;
 
+/**
+ *
+ * @param pxTask
+ * @param xTc
+ */
 void vSlackCalculateSlack_fixed1( HstTCB_t * pxTask, const TickType_t xTc );
 
+/**
+ *
+ * @param pxTask
+ * @param xTicks
+ */
 void vSlackGainSlack( const HstTCB_t * pxTask, const TickType_t xTicks ); //  __attribute__((always_inline));
+
+/**
+ *
+ * @param pxTask
+ * @param xTicks
+ */
 void vSlackDecrementTasksSlack( const HstTCB_t * pxTask, const TickType_t xTicks );
+
+/**
+ *
+ * @param xTicks
+ */
 void vSlackDecrementAllTasksSlack( const TickType_t xTicks );
+
+/**
+ *
+ * @param xAvailableSlack
+ */
 void vSlackUpdateAvailableSlack( BaseType_t * xAvailableSlack );
 
+/**
+ *
+ * @return
+ */
 BaseType_t vSlackCalculateTasksWcrt( void );
 
 #if defined (__cplusplus)
