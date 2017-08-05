bd5108b629b0bc91e26ef947d4db557c6eff7699
fepz <franpaez@gmail.com>
2017-08-05 18:06:51 -0300

    Influde timer configuration only if it is enabled.

--- FreeRTOS/v9.0.0/config/FreeRTOSConfig.h
+++ FreeRTOS/v9.0.0/config/FreeRTOSConfig.h
@@ -113,9 +113,11 @@ extern uint32_t SystemCoreClock;
 
 /* Software timer definitions. */
 #define configUSE_TIMERS				0
+#if ( configUSE_TIMERS == 1 )
 #define configTIMER_TASK_PRIORITY		( 2 )
 #define configTIMER_QUEUE_LENGTH		5
 #define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
+#endif
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
